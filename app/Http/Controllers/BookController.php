147cefe11b82a74f7ff4d47065ea6a12a784b177
adsyam <yambaoadrianne@gmail.com>
2023-11-13 18:40:04 +0800

    CHANGE: Layout + Search fn

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $title = $request->input("title");
+
+        $books = Book::when(
+            $title,
+            fn($query, $title) => $query->title($title)
+        )->get();
+
+        return view("books.index", ['books' => $books]);
+        // compact() finds a variable w/ the name indicated into an array
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,17 +2,59 @@
 
 namespace App\Models;
 
+use DateTime;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     use HasFactory;
 
-    public function reviews()
+    // Defines a one-to-many relationship between a book and its reviews.
+    public function reviews(): HasMany
     {
         return $this->hasMany(Review::class);
-        // hasMany() method defines a one to many relationship between book and the reviews
-        // it basically says that the book can have many reviews
+    }
+
+    // Filters books based on their title.
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    // Retrieves popular books with the most reviews within a specified date range.
+    public function scopePopular(Builder $query, ?DateTime $from = null, ?DateTime $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to),
+        ])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    // Retrieves highest rated books based on the average rating of their reviews.
+    public function scopeHighestRated(Builder $query, ?DateTime $from = null, ?DateTime $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+        // use having when working w/ aggregate functions like min, max, avg
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
